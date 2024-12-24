725374066499be2be12fd4292912b3da904166c8
TheBizzle <jason.bertsche@gmail.com>
2024-12-24 16:06:49 -0600

    Bugfix: Avoid `pcolor` import error

--- src/main/PColorImporter.scala
+++ src/main/PColorImporter.scala
@@ -49,7 +49,7 @@ object PColorsImporter {
 
     import world.{ minPxcor, minPycor, patchSize, worldWidth, worldHeight }
 
-    def genCoords(patchSize: Double, ratio: Double, worldDim: Int, imageDim: Int): (Int, Int, Map[Int, Int]) = {
+    def genCoords(patchSize: Double, ratio: Double, worldDim: Int, imageDim: Int): (Int, Map[Int, Int]) = {
 
       val worldPixelDim  = patchSize * worldDim
       val scaledImageDim = imageDim * ratio
@@ -63,7 +63,7 @@ object PColorsImporter {
           patchNum => patchNum -> StrictMath.floor((patchNum - startPatch) * dimRatio).toInt
         }.toMap
 
-      (startPatch, endPatch, startPixels)
+      (endPatch, startPixels)
 
     }
 
@@ -77,11 +77,11 @@ object PColorsImporter {
     val yRatio = patchSize * 1e10 * worldHeight / imageHeight / 1e10
     val ratio  = StrictMath.min(xRatio, yRatio)
 
-    val (xStart, xEnd, xStarts) = genCoords(patchSize, ratio, worldWidth , imageWidth )
-    val (yStart, yEnd, yStarts) = genCoords(patchSize, ratio, worldHeight, imageHeight)
+    val (xEnd, xStarts) = genCoords(patchSize, ratio, worldWidth , imageWidth )
+    val (yEnd, yStarts) = genCoords(patchSize, ratio, worldHeight, imageHeight)
 
     val updates =
-      for (xcor <- xStart until xEnd; ycor <- yStart until yEnd) yield {
+      for (xcor <- 0 until xEnd; ycor <- 0 until yEnd) yield {
 
         val minX = xStarts(xcor)
         val minY = yStarts(ycor)
